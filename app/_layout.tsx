aaec29c217df372f8d194e7af023d310e6f16a18
sonnv1912 <sonnv1912@gmail.com>
2025-04-18 22:26:40 +0700

    Wrap Stack component in DefaultLayout for consistent layout structure

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,12 @@
 import '../assets/styles/main.css';
 
+import { DefaultLayout } from '@layouts/default/default-layout';
 import { Stack } from 'expo-router';
 
 export default function RootLayout() {
-   return <Stack />;
+   return (
+      <DefaultLayout>
+         <Stack />
+      </DefaultLayout>
+   );
 }
