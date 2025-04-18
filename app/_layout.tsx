05cb689e193904a78c37e50c951367bcbdb31308
sonnv1912 <sonnv1912@gmail.com>
2025-04-18 19:39:43 +0700

    Refactor configuration files and improve code formatting for consistency

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,7 @@
-import "../assets/styles/main.css";
+import '../assets/styles/main.css';
 
-import { Stack } from "expo-router";
+import { Stack } from 'expo-router';
 
 export default function RootLayout() {
-  return <Stack />;
+   return <Stack />;
 }
