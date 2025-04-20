12664f84a9e9140fa00e2a10d6b712c3f9550e59
sonnv1912 <sonnv1912@gmail.com>
2025-04-20 11:15:24 +0700

    Add React Query integration with QueryClient setup and wrap RootLayout in QueryClientProvider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,17 +1,21 @@
-import '../assets/styles/main.css';
-import '../configs/i18n/index';
-
+import { queryClient } from '@configs/query-client';
+import { QueryClientProvider } from '@tanstack/react-query';
 import { DefaultLayout } from '@layouts/default/default-layout';
 import { Stack } from 'expo-router';
+
+import '../assets/styles/main.css';
+import '../configs/i18n/index';
 
 export default function RootLayout() {
    return (
       <DefaultLayout>
-         <Stack
-            screenOptions={{
-               headerShown: false,
-            }}
-         />
+         <QueryClientProvider client={queryClient}>
+            <Stack
+               screenOptions={{
+                  headerShown: false,
+               }}
+            />
+         </QueryClientProvider>
       </DefaultLayout>
    );
 }
